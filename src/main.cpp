fc43b60e2db6357c1a0f16ef5c8f8365fd5758a5
Connor Garey <connorgarey1999@gmail.com>
2026-07-10 19:38:54 +0100

    Added each register to main.
    Renamed registers.
    
    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
+#include "register.hpp"
+
 #include <cstdlib>
 
-auto main() -> int {
+auto main() -> int
+{
+  Chip8::Register<std::uint8_t> Reg0{};
+  Chip8::Register<std::uint8_t> Reg1{};
+  Chip8::Register<std::uint8_t> Reg2{};
+  Chip8::Register<std::uint8_t> Reg3{};
+  Chip8::Register<std::uint8_t> Reg4{};
+  Chip8::Register<std::uint8_t> Reg5{};
+  Chip8::Register<std::uint8_t> Reg6{};
+  Chip8::Register<std::uint8_t> Reg7{};
+  Chip8::Register<std::uint8_t> Reg8{};
+  Chip8::Register<std::uint8_t> Reg9{};
+  Chip8::Register<std::uint8_t> RegA{};
+  Chip8::Register<std::uint8_t> RegB{};
+  Chip8::Register<std::uint8_t> RegC{};
+  Chip8::Register<std::uint8_t> RegD{};
+  Chip8::Register<std::uint8_t> RegE{};
+  Chip8::Register<std::uint8_t> RegF{};
+  Chip8::Register<std::uint16_t> RegI{};
+
   return EXIT_SUCCESS;
 }
