831803aa48d2534b571061c3acac2c4c3f6973d1
Connor Garey <connorgarey1999@gmail.com>
2026-07-12 18:49:13 +0100

    load_lua_configuration_file() func
    .
    
    .
    
    .

--- src/lua-instance.cpp
+++ src/lua-instance.cpp
@@ -1,8 +1,27 @@
 #include "lua-instance.hpp"
 
 #include <cassert>
+#include <cstdlib>
+#include <expected>
+#include <filesystem>
 #include <format>
+#include <lauxlib.h>
 #include <stdexcept>
+#include <string_view>
+
+namespace
+{
+auto load_lua_configuration_file() -> std::expected<std::filesystem::path, std::string_view>
+{
+  const auto *custom_config_dir{std::getenv("CHIP8_LUA_CONFIG_DIR")};
+  if (custom_config_dir == nullptr)
+  {
+    return std::unexpected("\"CHIP8-LUA-CONFIG_DIR\" is nullptr!");
+  }
+
+  return std::filesystem::path{custom_config_dir} / "config.lua";
+}
+} // namespace
 
 Chip8::LuaInstance::LuaInstance() : lua_{luaL_newstate(), &lua_close}
 {
@@ -12,7 +31,9 @@ Chip8::LuaInstance::LuaInstance() : lua_{luaL_newstate(), &lua_close}
   }
 
   luaL_openlibs(lua_.get());
-  if (const auto lres = luaL_dofile(lua_.get(), "config.lua"); static_cast<int>(lres) != LUA_OK) [[unlikely]]
+
+  if (const auto lres = luaL_dofile(lua_.get(), load_lua_configuration_file().value_or("config.lua").c_str());
+      static_cast<int>(lres) != LUA_OK) [[unlikely]]
   {
     throw std::runtime_error(std::format("Failed to read Lua config file! Error: {}", lres));
   }
