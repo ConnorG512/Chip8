460bbe771f734e15aeb165ac4e6aa8010362f1bf
Connor Garey <connorgarey1999@gmail.com>
2026-07-12 18:37:16 +0100

    Adjusted window scale.

--- lua/config.lua
+++ lua/config.lua
@@ -2,7 +2,7 @@
 
 -- Base resolution of a Chip8 interpreter is 64 x 32. 
 -- Adjust to comfortable window scale.
-window_scale = 12;
+window_scale = 18;
 
 -- Interpreter clock speed.
 speed = 7000;
