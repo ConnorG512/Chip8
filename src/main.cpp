65c35af6dc6aae2adb8af1b45adba89ac128e1ef
Connor Garey <connorgarey1999@gmail.com>
2026-07-15 12:41:43 +0100

    Fixed error messages.

--- src/main.cpp
+++ src/main.cpp
@@ -58,11 +58,12 @@ auto main() -> int
             .or_else(
                 [&](const Chip8::MemBuf::LoadAppErr &err) -> auto
                 {
-                  std::cerr << "Failed to load default App. Aborting. Error: " << static_cast<std::uint32_t>(std::to_underlying(err)) << '\n';
+                  std::cerr << "Failed to load app from Lua string, Aborting. Error: " << static_cast<std::uint32_t>(std::to_underlying(err)) << '\n';
                   return mem_buf.load_app_into_buffer("test-1");
                 });
     if (!result.has_value())
     {
+      std::cerr << "Failed to load default app, Aborting.\n";
       return EXIT_FAILURE;
     }
 
