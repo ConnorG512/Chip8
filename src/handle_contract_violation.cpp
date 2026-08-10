58888a51c8847a1b059f052ba57bad10d2fafe87
Connor Garey <connorgarey1999@gmail.com>
2026-08-11 00:40:45 +0100

    Additions to contract_assert

--- src/handle_contract_violation.cpp
+++ src/handle_contract_violation.cpp
@@ -1,7 +1,11 @@
 #include <contracts>
 #include <print>
+#include <source_location>
 
 void handle_contract_violation(const std::contracts::contract_violation &cv)
 {
-  std::println("Found contract violation {}", cv.comment());  
+  const std::source_location location {cv.location()};
+
+  std::println("Found contract violation! {}.", cv.comment());  
+  std::println("Detail: {}\n{}\n{}.", location.file_name(), location.line(), location.function_name());
 }
