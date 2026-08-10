b0938fef82e07027044e5fd2a1e8b488079b3cec
Connor Garey <connorgarey1999@gmail.com>
2026-08-10 22:20:30 +0100

    Making use of contracts in WindowWH strong type.
    remvoed unneeded assert
    
    Removed headers
    
    Format
    
    Added pre conditions when getting a new value  as well as templated
    Dimensions
    
    Place preconditions before initialiser list.
    
    Static cast out the getter to std::uint32_t
    
    There is no std::static_cast
    
    Make sure to cast to the correct type.

--- include/window-wh.hpp
+++ include/window-wh.hpp
@@ -3,37 +3,44 @@
 #include "chip8-spec.hpp"
 
 #include <cassert>
+#include <concepts>
 #include <cstdint>
-#include <initializer_list>
-#include <utility>
+#include <limits>
 
 namespace Chip8
 {
-struct Dimensions
-{
-  std::uint32_t width{};
-  std::uint32_t height{};
-};
+template <typename T>
+concept DimensionsT = std::same_as<T, std::uint32_t> || std::same_as<T, std::int32_t>;
 
+template <DimensionsT T = std::uint32_t> struct Dimensions
+{
+  T width{};
+  T height{};
+};
 
 class WindowWH
 {
 public:
-  explicit constexpr WindowWH(Dimensions window_dim) noexcept : width_{window_dim.width}, height_{window_dim.height}
+  explicit constexpr WindowWH(Dimensions<std::uint32_t> window_dim) noexcept
+      pre(window_dim.width >= Chip8::Spec::screen_width)
+      pre(window_dim.height >= Chip8::Spec::screen_height)
+      pre(window_dim.width <= std::numeric_limits<std::int32_t>::max())
+      pre(window_dim.height <= std::numeric_limits<std::int32_t>::max())
+      : width_{window_dim.width}, height_{window_dim.height} 
   {
-    for (auto dim : {window_dim.width, window_dim.height})
-    {
-      assert(std::cmp_greater_equal(dim, Spec::minimum_window_size));
-    }
   }
 
-  [[nodiscard]] constexpr auto value() const -> Dimensions { return {.width = width_, .height = height_}; };
-  constexpr auto operator=(Dimensions value) -> WindowWH &
+  [[nodiscard]] constexpr auto value() const -> Dimensions<std::int32_t>
   {
-    for (auto dim : {value.width, value.height})
-    {
-      assert(std::cmp_greater_equal(dim, Spec::minimum_window_size));
-    }
+    return {.width = static_cast<std::int32_t>(width_), .height = static_cast<std::int32_t>(height_)};
+  };
+
+  constexpr auto operator=(Dimensions<std::uint32_t> value) -> WindowWH &
+    pre(value.width >= Chip8::Spec::screen_width)
+    pre(value.height >= Chip8::Spec::screen_height)
+    pre(value.width <= std::numeric_limits<std::int32_t>::max())
+    pre(value.height <= std::numeric_limits<std::int32_t>::max())
+  {
     width_ = value.width;
     height_ = value.height;
     return *this;
