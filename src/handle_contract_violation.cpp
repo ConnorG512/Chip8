e8f67c14f38737304f7b4e66786ad3d23087709d
Connor Garey <connorgarey1999@gmail.com>
2026-08-11 01:12:05 +0100

    Better error formatting.

--- src/handle_contract_violation.cpp
+++ src/handle_contract_violation.cpp
@@ -6,6 +6,6 @@ void handle_contract_violation(const std::contracts::contract_violation &cv)
 {
   const std::source_location location {cv.location()};
 
-  std::println("Found contract violation! {}.", cv.comment());  
-  std::println("Detail: {}\n{}\n{}.", location.file_name(), location.line(), location.function_name());
+  std::println("Found contract violation! Assert: [{}].", cv.comment());  
+  std::println("File: [{}] at line [{}]\nFunction: {}.", location.file_name(), location.line(), location.function_name());
 }
